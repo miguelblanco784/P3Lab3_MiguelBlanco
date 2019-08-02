53e7d2c57692826a71840c8c38f6251c23ab99dc
miguelblanco784 <miguelrobertoblanco@gmail.com>
2019-08-02 15:06:00 -0600

    Termine las Clases

---
+++ Cola.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<iomanip> 
+#include <string.h>
+#include "Computadora.cpp"
+#include <vector>
+#include <bits/stdc++.h>
+
+#ifndef COLA_CPP
+#define COLA_CPP
+using namespace std;
+class Cola{
+	private:
+		
+	public:
+		
+};
+#endif
---
+++ Orejas.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<iomanip> 
+#include <string.h>
+#include "Computadora.cpp"
+#include <vector>
+#include <bits/stdc++.h>
+
+#ifndef OREJAS_CPP
+#define OREJAS_CPP
+using namespace std;
+class Orejas{
+	private:
+		
+	public:
+		
+};
+#endif
